6f0d449e3e1795e906cd256790be4c7635f2b9b9
Carlo Sciolla <carlo.sciolla@gmail.com>
2011-04-20 17:11:33 +0200

    More features

--- src/main/clojure/spring/surf/webscript.clj
+++ src/main/clojure/spring/surf/webscript.clj
@@ -5,7 +5,17 @@
   (run [this model] "Run the webscript and return the model"))
 
 (defn return
+  "Updates the view-model with the provided one"
   [model view-model]
   (let [view-model-orig (.get model "model")]
     (.putAll view-model-orig view-model)
     model))
+
+(defn with-model
+  "Invoke a WebScript and returns the original model with its viewModel
+   updated with what the WebScript returned. It should cover most cases
+   needs."
+  [model ^WebScript ws]
+  (let [view-model (into {} (.get "model" model))
+        result (merge view-model (.run ws model))]
+    (return model result)))
--- src/main/java/org/springframework/extensions/webscripts/processor/ClojureScriptProcessorFactory.java
+++ src/main/java/org/springframework/extensions/webscripts/processor/ClojureScriptProcessorFactory.java
@@ -1,21 +1,3 @@
-/**
- * Copyright (C) 2005-2009 Alfresco Software Limited.
- *
- * This file is part of the Spring Surf Extension project.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.springframework.extensions.webscripts.processor;
 
 import org.apache.commons.logging.Log;
@@ -26,7 +8,7 @@ import org.springframework.extensions.webscripts.ScriptProcessor;
 import org.springframework.extensions.webscripts.ScriptProcessorFactory;
 
 /**
- * @author muzquiano
+ * @author Carlo Sciolla &lt;skuro@skuro.tk&gt;
  */
 public class ClojureScriptProcessorFactory implements ScriptProcessorFactory, ApplicationContextAware
 {
